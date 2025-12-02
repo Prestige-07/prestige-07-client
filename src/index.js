4af4905e7d3a2bf64905cb2cad847502473d75ca
Yevhenii-Lukianenko <zhenyalukyanenko17@gmail.com>
2025-12-02 17:42:02 +0200

    Змінив basename, щоб працював гіт деплой

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  // BrowserRouter,
-  HashRouter,
-} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
@@ -15,14 +12,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        {/* <BrowserRouter basename="">
+        <BrowserRouter basename="/prestige-07-client">
           <App />
           <Toaster />
-        </BrowserRouter> */}
-        <HashRouter>
-          <App />
-          <Toaster />
-        </HashRouter>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
